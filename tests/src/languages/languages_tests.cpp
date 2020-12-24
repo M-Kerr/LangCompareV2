42011d894b3b733c562d9993e7507c7d9bbfa944
MK <34846459+M-Kerr@users.noreply.github.com>
2020-12-24 00:43:55 -0500

    execute() tests added

--- tests/src/languages/languages_tests.cpp
+++ tests/src/languages/languages_tests.cpp
@@ -13,45 +13,29 @@ void code_execute(QString language, QString file)
     Code *code = code_factory(language, file);
     ASSERT_TRUE(code != nullptr) << "Failed to create Code *";
 
+    EXPECT_TRUE(code->execute(fd[0], fd[1])) << "execute() failed";
+
     ASSERT_FALSE(close(fd[0]) < 0) << "Failed to close pipe read\n";
     ASSERT_FALSE(close(fd[1]) < 0) << "Failed to close pipe write\n";
 }
 
 TEST(CppCodeTests, CanExecute)
 {
-    int fd[2];
-
-    qInfo() << "Current Working Dir: ";
-    qInfo() << QDir::currentPath();
-
-
-    // We are failing because the code/ directory is not here.
-    // We aren't finding any files
-
-    //cpp->execute(fd[0], fd[1]);
+    code_execute("c++", "hello_world.cpp");
 }
 
 // Tests executing files in subdirectories
 TEST(CppCodeTests, CanExecuteModules)
 {
-    Code *cpp = code_factory("c++", "templating/templating.cpp");
-
-    int fd[2];
-
-    //cpp->execute(fd[0], fd[1]);
+    code_execute("c++", "templating/templating.cpp");
 }
 
 TEST(PythonCodeTests, CanExecute)
 {
-    Code *py = code_factory("python", "hello_world.py");
-
-    int fd[2];
-
-    //py->execute(fd[0], fd[1]);
-
+    code_execute("python", "hello_world.py");
 }
 
 TEST(PythonCodeTests, CanExecuteModules)
 {
-
+    FAIL() << "***Not yet implemented***";
 }
