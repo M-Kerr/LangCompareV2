e8b0c548c722ce9fe1463ce2933996477def6848
MK <34846459+M-Kerr@users.noreply.github.com>
2020-12-29 02:43:35 -0500

    reduced stdout verbosity

--- src/languages/code_python.cpp
+++ src/languages/code_python.cpp
@@ -14,7 +14,7 @@ bool Code_Python::execute(int read_fd, int write_fd)
                         + " -iter " + QString::number(get_iters())
                         + " -limit " + QString::number(get_limit());
 
-    qInfo() << "\nExecuting Python " << output_file() << "...\n";
+    qInfo() << "\nExecuting Python " << file_name() << "...\n";
     int error_code = std::system(python_command.toLatin1());
     if (error_code)
     {
